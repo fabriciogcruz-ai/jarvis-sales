a6693512ea770ff9d0ee52b0bca7ed33ad326a35
bitinhoc <bitinnnn@gmail.com>
2025-07-13 03:24:43 -0300

    Usa undetected-chromedriver para deploy na Render

--- scraper/linkedin_scraper.py
+++ scraper/linkedin_scraper.py
@@ -1,7 +1,5 @@
+import undetected_chromedriver as uc
 from linkedin_scraper import Person, Company, actions
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 from typing import List, Tuple
 
 def scrape_profile_and_company(
@@ -12,14 +10,12 @@ def scrape_profile_and_company(
     cookie: str | None = None
 ) -> Tuple[str, str, str]:
 
-    options = webdriver.ChromeOptions()
-    options.add_argument("--log-level=3")
-    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    options = uc.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Chrome(
-        service=ChromeService(ChromeDriverManager().install()),
-        options=options,
-    )
+    driver = uc.Chrome(options=options)
 
     try:
         if cookie:
